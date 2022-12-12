5640683a71308a32a98e14fee9a67c09c694f1b5
DenisZaborskih <dzabors@mail.ru>
2022-12-12 23:24:27 +0700

    faster

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double my_pow(double result, unsigned int numpow) {
+	return pow(result, numpow);
+}
+
+int main() {
+	double number;
+	unsigned int deg;
+	cout << "Enter a number and a degree" << endl;
+	cin >> number;
+	cin >> deg;
+	cout << "Answer is: " << my_pow(number, deg) << endl;
+	return 0;
+}
