062161da1ac7176768e44d72a8d6f940aa203374
DenisZaborskih <dzabors@mail.ru>
2022-12-12 22:48:27 +0700

    what

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double my_pow(double result, unsigned int numpow) {
+	double temp = result;
+	for (int i = 1; i < numpow; i++) result *= temp;
+	return result;
+}
+
+int main() {
+	double number;
+	unsigned int deg;
+	cout << "Enter a number and a degree" << endl;
+	cin >> number;
+	cin >> deg;
+	cout << "Answer is: " << my_pow(number, deg) << endl;
+	return 0;
+}
