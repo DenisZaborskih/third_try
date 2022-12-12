18f2e9c428a68cc235d14fa5d4f1391188d63cdb
DenisZaborskih <dzabors@mail.ru>
2022-12-12 23:07:24 +0700

    who

---
+++ fast.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double my_pow(double result, int numpow) {
+	double temp = result;
+	if (numpow > 0) for (int i = 1; i < numpow; i++) result *= temp;
+	else if (numpow < 0) for (int i = 1; i < numpow; i++) result /= temp;
+	else result = 1;
+	return result;
+}
+
+int main() {
+	double number;
+	int deg;
+	cout << "Enter a number and a degree" << endl;
+	cin >> number;
+	cin >> deg;
+	cout << "Answer is: " << my_pow(number, deg) << endl;
+	return 0;
+}
